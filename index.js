fe79421d5f1284c3ea09fb244bc969932316f287
Grelgn <grelgn@hotmail.com>
2024-04-01 21:34:54 +0300

    Route to appropriate pages

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	res.end("<h1>test</h1>");
+	if (req.url === "/") {
+		fs.readFile(path.join(__dirname, "index.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+	} else if (req.url === "/about") {
+		fs.readFile(path.join(__dirname, "about.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+    }
+    else if (req.url === "/contact-me") {
+		fs.readFile(path.join(__dirname, "contact-me.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+    } else {
+        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(404, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+    }
 });
 
 server.listen(8080, () => {
